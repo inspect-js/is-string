30675ecb5c5cc43873918661a414a1d0f8b77325
Jordan Harband <ljharb@gmail.com>
2015-01-29 23:39:15 -0800

    If @@toStringTag is not present, use the old-school Object#toString test.

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ var tryStringObject = function tryStringObject(value) {
 		return false;
 	}
 };
+var toStr = Object.prototype.toString;
+var strClass = '[object String]';
+var hasToStringTag = typeof Symbol === 'function' && typeof Symbol.toStringTag === 'symbol';
 
 module.exports = function isString(value) {
 	if (typeof value === 'string') { return true; }
 	if (typeof value !== 'object') { return false; }
-	return tryStringObject(value);
+	return hasToStringTag ? tryStringObject(value) : toStr.call(value) === strClass;
 };
